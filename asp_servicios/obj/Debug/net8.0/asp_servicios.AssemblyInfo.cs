4be34399dc9e7722c9851d04ceb03b6f088f5518
Javier <javiertorresmvm@gmail.com>
2025-05-15 19:36:59 -0500

    Corregir apartado en ordenes

--- asp_servicios/obj/Debug/net8.0/asp_servicios.AssemblyInfo.cs
+++ asp_servicios/obj/Debug/net8.0/asp_servicios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp_servicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55be2dc775fc8199546277e419324ac32743089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07739e42c7c12ae5a76afd8306ecbc88e5e38e7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp_servicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp_servicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
