1fc7bb83adc90cc2ca108c4859d8867ccbe317e1
Javier <javiertorresmvm@gmail.com>
2025-05-16 23:59:48 -0500

    Json ignore para evitar bucle en propiedades de navegacion

--- lib_dominio/Entidades/Marcas.cs
+++ lib_dominio/Entidades/Marcas.cs
@@ -1,10 +1,12 @@
-﻿namespace lib_dominio.Entidades
+﻿using Newtonsoft.Json;
+namespace lib_dominio.Entidades
 {
     public class Marcas
     {
         public int Id { get; set; }
         public string? NombreMarca { get; set; }
         public string? PaginaWeb { get; set; }
+        [JsonIgnore]
         public List<Discos>? Discos { get; set; }
     }
 }
